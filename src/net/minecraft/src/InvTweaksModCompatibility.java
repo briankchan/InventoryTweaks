99a2d57659d6fa0a32ae65bee2ff8eb6e4ddb9d6
Lordmau5 <mau5@aegcrafter.de>
2012-12-30 14:45:58 +0100

    Compatibility for Ender Chest and Forestry
    And a bit of code rewrite ;)

--- src/net/minecraft/src/InvTweaksModCompatibility.java
+++ src/net/minecraft/src/InvTweaksModCompatibility.java
@@ -17,48 +17,49 @@ public class InvTweaksModCompatibility
     {
         this.obf = var1;
     }
-
+    
     public boolean isSpecialChest(GuiScreen guiScreen) {
-        return is(guiScreen, "GuiAlchChest") // Equivalent Exchange
-            || is(guiScreen, "GuiCondenser") // Equivalent Exchange
-        	|| is(guiScreen, "GUIChest") // Iron chests (formerly IC2)
-                || is(guiScreen, "GuiMultiPageChest") // Multi Page chest
-                || is(guiScreen, "GuiGoldSafe") // More Storage
-                || is(guiScreen, "GuiLocker")
-                || is(guiScreen, "GuiDualLocker")
-                || is(guiScreen, "GuiSafe") 
-                || is(guiScreen, "GuiCabinet") 
-                || is(guiScreen, "GuiTower")
-                || is(guiScreen, "GuiBufferChest") // Red Power 2
-                || is(guiScreen, "GuiRetriever") // Red Power 2
-                || is(guiScreen, "GuiItemDetect") // Red Power 2
-                || is(guiScreen, "GuiAlloyFurnace") // Red Power 2
-                || is(guiScreen, "GuiDeploy") // Red Power 2
-                || is(guiScreen, "GuiSorter") // Red Power 2
-                || is(guiScreen, "GuiFilter") // Red Power 2
-                || is(guiScreen, "GuiAdvBench") // Red Power 2
-                || is(guiScreen, "GuiEject") // Red Power 2
-                || is(guiScreen, "GuiPersonalChest")
-                || is(guiScreen, "GuiNuclearReactor") // IC2
-                || is(guiScreen, "GuiEnderChest") // EnderChest
-                || is(guiScreen, "GuiColorBox")
-                || is(guiScreen, "GuiLinkedColorBox") // ColorBox
-                || is(guiScreen, "FC_GuiChest") // Metallurgy
-                || is(guiScreen, "FM_GuiMintStorage") // Metallurgy
-                || is(guiScreen, "GuiChestTFC") // TerraFirmaCraft
+        return is_GuiScreen(guiScreen, "GuiAlchChest") // Equivalent Exchange
+            || is_GuiScreen(guiScreen, "GuiCondenser") // Equivalent Exchange
+        	|| is_GuiScreen(guiScreen, "GUIChest") // Iron chests (formerly IC2)
+                || is_GuiScreen(guiScreen, "GuiMultiPageChest") // Multi Page chest
+                || is_GuiScreen(guiScreen, "GuiGoldSafe") // More Storage
+                || is_GuiScreen(guiScreen, "GuiLocker")
+                || is_GuiScreen(guiScreen, "GuiDualLocker")
+                || is_GuiScreen(guiScreen, "GuiSafe") 
+                || is_GuiScreen(guiScreen, "GuiCabinet") 
+                || is_GuiScreen(guiScreen, "GuiTower")
+                || is_GuiScreen(guiScreen, "GuiBufferChest") // Red Power 2
+                || is_GuiScreen(guiScreen, "GuiRetriever") // Red Power 2
+                || is_GuiScreen(guiScreen, "GuiItemDetect") // Red Power 2
+                || is_GuiScreen(guiScreen, "GuiAlloyFurnace") // Red Power 2
+                || is_GuiScreen(guiScreen, "GuiDeploy") // Red Power 2
+                || is_GuiScreen(guiScreen, "GuiSorter") // Red Power 2
+                || is_GuiScreen(guiScreen, "GuiFilter") // Red Power 2
+                || is_GuiScreen(guiScreen, "GuiAdvBench") // Red Power 2
+                || is_GuiScreen(guiScreen, "GuiEject") // Red Power 2
+                || is_GuiScreen(guiScreen, "GuiPersonalChest")
+                || is_GuiScreen(guiScreen, "GuiNuclearReactor") // IC2
+                //|| is_GuiScreen(guiScreen, "GuiEnderChest") // EnderChest Old
+                || is_GuiScreen(guiScreen, "GuiColorBox")
+                || is_GuiScreen(guiScreen, "GuiLinkedColorBox") // ColorBox
+                || is_GuiScreen(guiScreen, "FC_GuiChest") || is_GuiScreen(guiScreen, "FM_GuiMintStorage") // Metallurgy
+                || is_GuiScreen(guiScreen, "GuiChestTFC") // TerraFirmaCraft
                 
                 //Added by Lordmau5
-                || is(guiScreen, "GuiBackpack") // Backpack Mod
-                || is(guiScreen, "GuiBag") // Red Power 2 Canvas Bag
-                || is(guiScreen, "GuiReinforcedChest") // Better Storage Mod
+                || is_GuiScreen(guiScreen, "GuiBackpack") // Backpack Mod or Forestry Tier 1
+                || is_GuiScreen(guiScreen, "GuiBag") // Red Power 2 Canvas Bag
+                || is_GuiScreen(guiScreen, "GuiReinforcedChest") // Better Storage Mod
+                || is_GuiScreen(guiScreen, "GuiEnderStorage") // Ender Storage / Ender Chest
+                || is_GuiScreen(guiScreen, "GuiBackpackT2") || is_GuiScreen(guiScreen, "GuiForestry") // Forestry
           ;
     }
 
     public int getSpecialChestRowSize(GuiContainer var1, int var2)
     {
-        if (!is(var1, "GuiAlchChest") && !is(var1, "GuiCondenser"))
+        if (!is_GuiScreen(var1, "GuiAlchChest") && !is_GuiScreen(var1, "GuiCondenser"))
         {
-            if (is(var1, "GUIChest"))
+            if (is_GuiScreen(var1, "GUIChest"))
             {
                 try
                 {
@@ -71,32 +72,32 @@ public class InvTweaksModCompatibility
             }
             else
             {
-                if (is(var1, "GuiMultiPageChest"))
+                if (is_GuiScreen(var1, "GuiMultiPageChest"))
                 {
                     return 13;
                 }
 
-                if (is(var1, "GuiLocker") || is(var1, "GuiDualLocker") || is(var1, "GuiTower"))
+                if (is_GuiScreen(var1, "GuiLocker") || is_GuiScreen(var1, "GuiDualLocker") || is_GuiScreen(var1, "GuiTower"))
                 {
                     return 8;
                 }
 
-                if (is(var1, "GuiBufferChest"))
+                if (is_GuiScreen(var1, "GuiBufferChest"))
                 {
                     return 4;
                 }
 
-                if (is(var1, "GuiSorter"))
+                if (is_GuiScreen(var1, "GuiSorter"))
                 {
                     return 8;
                 }
 
-                if (is(var1, "GuiRetriever") || is(var1, "GuiItemDetect") || is(var1, "GuiAlloyFurnace") || is(var1, "GuiDeploy") || is(var1, "GuiFilter") || is(var1, "GuiEject"))
+                if (is_GuiScreen(var1, "GuiRetriever") || is_GuiScreen(var1, "GuiItemDetect") || is_GuiScreen(var1, "GuiAlloyFurnace") || is_GuiScreen(var1, "GuiDeploy") || is_GuiScreen(var1, "GuiFilter") || is_GuiScreen(var1, "GuiEject"))
                 {
                     return 3;
                 }
 
-                if (is(var1, "GuiNuclearReactor"))
+                if (is_GuiScreen(var1, "GuiNuclearReactor"))
                 {
                     return (this.obf.getSlots(this.obf.getContainer(var1)).size() - 36) / 6;
                 }
@@ -112,7 +113,7 @@ public class InvTweaksModCompatibility
 
     public boolean isChestWayTooBig(GuiScreen var1)
     {
-        return is(var1, "GuiAlchChest") || is(var1, "GuiMultiPageChest") || is(var1, "GUIChest") || is(var1, "FC_GuiChest");
+        return is_GuiScreen(var1, "GuiAlchChest") || is_GuiScreen(var1, "GuiMultiPageChest") || is_GuiScreen(var1, "GUIChest") || is_GuiScreen(var1, "FC_GuiChest") || is_GuiScreen(var1, "GuiReinforcedChest") || is_GuiScreen(var1, "GuiBackpackT2");
     }
 
     public boolean isSpecialInventory(GuiScreen var1)
@@ -132,11 +133,11 @@ public class InvTweaksModCompatibility
         HashMap var3 = new HashMap();
         List var4 = this.obf.getSlots(var2);
 
-        if (is(var1, "GuiCondenser"))
+        if (is_GuiScreen(var1, "GuiCondenser"))
         {
             var3.put(InvTweaksContainerSection.CHEST, var4.subList(1, var4.size() - 36));
         }
-        else if (is(var1, "GuiAdvBench"))
+        else if (is_GuiScreen(var1, "GuiAdvBench"))
         {
             var3.put(InvTweaksContainerSection.CRAFTING_IN, var4.subList(0, 9));
             var3.put(InvTweaksContainerSection.CRAFTING_OUT, var4.subList(9, 10));
@@ -146,7 +147,7 @@ public class InvTweaksModCompatibility
         return var3;
     }
 
-    private static final boolean is(GuiScreen var0, String var1)
+    private static final boolean is_GuiScreen(GuiScreen var0, String var1)
     {
         try
         {
